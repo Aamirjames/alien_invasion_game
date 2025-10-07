f0762a0167f511120c3ce113f10bdfa8b3a09b42
AamirKhan <aamirjames006@gmail.com>
2025-10-07 11:10:55 +0500

    Displayed player spaceship on the display window

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,24 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
     # overall class to manage the game asstes and behaviours
     def __init__(self):
-        # Initialize the game, and create game resources(bacgroud settings to run the game).
+        # Initialize the game, and create game resources(backgroud settings to run the game).
         pygame.init()
+        self.settings = Settings()  # For using settings
 
-        # to set a display window
-        self.screen = pygame.display.set_mode((1200, 800))
+        # to set a display window(fetched width and height from settings)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)  # To display player spaceship
+
+        # Set the background color(screen background).
+        self.bg_color = (230, 230, 230)      # Almost black
 
     def run_game(self):
         # Start the main loop for the game.
@@ -20,8 +28,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
+            # Redraw the screen during each pass through the loop(after changing the background color).
+            self.screen.fill(self.settings.bg_color)  # Fetched from settings
+            self.ship.blitme()  # To draw the spaceship at it current location
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    '''A class for storing all the settings of the game'''
+
+    def __init__(self):
+        '''Initialize game settings'''
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
