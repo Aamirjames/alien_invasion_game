0b5f4929a66f5c931e10b0ddd9c8ea6c92a303fb
AamirKhan <aamirjames006@gmail.com>
2025-10-07 12:00:04 +0500

    Updated screen height to match screen size

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (200, 200, 230)
